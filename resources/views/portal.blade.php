ba60a4aa64ea7dba5a40abf71f06ada5ba8c770d
devdamo <minecraft.pop11@outlook.com>
2025-02-05 03:09:33 +0000

    removed logo partical.js to be added

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -1,13 +1,10 @@
-<section class="bg-white pt-10 dark:bg-[#1c1c20]">
+<section class="mt-30 bg-white pt-10 dark:bg-[#1c1c20]">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Jamie Dovaston</h1>
             <p class="max-w-2xl mb-6 font-bold text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                 <span id="typewriter" class="text-gray-300"></span><span id="cursor" class="text-white font-bold blink">|</span>
             </p>
-        </div>
-        <div class="hidden lg:mt-0 col-span-5 pl-10 w-[225px] h-[125px] lg:flex">
-            <img src="https://image.overtimehosting.cloud/u/WSa5Vl.png" alt="mockup">
         </div>
     </div>
 </section>
