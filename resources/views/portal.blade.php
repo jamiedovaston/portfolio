6c07945bf51d45ffe83bad265e2baf21941fa695
devdamo <jamiedovaston@outlook.com>
2025-02-11 14:04:51 +0000

    changes to navigation and portal

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -7,7 +7,7 @@
                         Jamie Dovaston
                     </h1>
                     <p class="max-w-2xl mb-8 font-bold text-gray-500 text-lg dark:text-gray-400">
-                        <span class="text-gray-300">Bsc/Hons Games Design and Programming Student at University of Staffordshire</span>
+                        <span class="text-gray-500">Bsc/Hons Games Design and Programming Student at University of Staffordshire</span>
                     </p>
                 </div>
                 <div class="w-16">
@@ -15,12 +15,12 @@
                 <div class="flex justify-center items-center w-full max-w-[200px] max-h-[200px]">
                     <svg class="w-full h-auto text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 274.58 164.09">
                         <g>
-                            <path class="fill-gray-700 dark:fill-gray-300" d="M118.84 0v27.12L46.98 68.61l23.27 13.44 48.59 28.06v27.11L23.27 82.04 0 68.61 86.29 18.79 118.84 0Z"></path>
-                            <path class="fill-gray-300 dark:fill-gray-700" d="M118.84 27.12v26.87L70.25 82.05 46.98 68.61 118.84 27.12Z"></path>
-                            <path class="fill-gray-300 dark:fill-gray-700" d="M118.84 137.22v26.87L0 95.48V68.61l23.27 13.43L118.84 137.22Z"></path>
-                            <path class="fill-gray-700 dark:fill-gray-300" d="M155.74 0v27.12l71.86 41.49-23.27 13.44-48.59 28.06v27.11l95.57-55.18 23.27-13.43L188.29 18.79 155.74 0Z"></path>
-                            <path class="fill-gray-300 dark:fill-gray-700" d="M155.74 27.12v26.87l48.59 28.06 23.27-13.44L155.74 27.12Z"></path>
-                            <path class="fill-gray-300 dark:fill-gray-700" d="M155.74 137.22v26.87l118.84-68.61V68.61l-23.27 13.43L155.74 137.22Z"></path>
+                            <path class="fill-gray-300" d="M118.84 0v27.12L46.98 68.61l23.27 13.44 48.59 28.06v27.11L23.27 82.04 0 68.61 86.29 18.79 118.84 0Z"></path>
+                            <path class="fill-gray-700" d="M118.84 27.12v26.87L70.25 82.05 46.98 68.61 118.84 27.12Z"></path>
+                            <path class="fill-gray-700" d="M118.84 137.22v26.87L0 95.48V68.61l23.27 13.43L118.84 137.22Z"></path>
+                            <path class="fill-gray-300" d="M155.74 0v27.12l71.86 41.49-23.27 13.44-48.59 28.06v27.11l95.57-55.18 23.27-13.43L188.29 18.79 155.74 0Z"></path>
+                            <path class="fill-gray-700" d="M155.74 27.12v26.87l48.59 28.06 23.27-13.44L155.74 27.12Z"></path>
+                            <path class="fill-gray-700" d="M155.74 137.22v26.87l118.84-68.61V68.61l-23.27 13.43L155.74 137.22Z"></path>
                         </g>
                     </svg>
                 </div>
